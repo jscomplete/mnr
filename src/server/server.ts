e5be1051e8b3a696eb2f02b3afe660dbcb36802d
Samer Buna <samerbuna@users.noreply.github.com>
2022-08-14 17:12:52 -0700

    1.3

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,19 @@
 import express from "express";
 
-console.log(express);
+const server = express();
+
+server.use(express.static("dist"));
+
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+  res.render("index", {
+    content: "EJS is <em>cool</em>!",
+  });
+});
+
+server.listen("8080", "0.0.0.0", () => {
+  console.info(
+    "Express server  is listening at http://0.0.0.0:8080",
+  );
+});
