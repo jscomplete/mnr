442cd3bde49e05cdabe13179f37b15aaf4303882
Samer Buna <samerbuna@users.noreply.github.com>
2022-08-14 19:16:52 -0700

    2.5

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-
 import Header from "./header";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
   return (
     <div className="container">
       <Header message="Naming Contests" />
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        {counter}
-      </button>
     </div>
   );
 };
