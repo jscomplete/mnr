4a6a07daada55cac9404c276282edeb2a8f47db2
Samer Buna <samerbuna@users.noreply.github.com>
2022-08-15 16:27:33 -0700

    3.8

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,16 +1,36 @@
 import express from "express";
 import cors from "cors";
 
+import { connectClient } from "./db";
+
 const router = express.Router();
 router.use(cors());
 
-import testData from "../test-data.json";
+router.get("/contests", async (req, res) => {
+  const client = await connectClient();
 
-router.get("/contests", (req, res) => {
-  // get the data from MongoDB
-  res.send({ contests: testData });
+  const contests = await client
+    .collection("contests")
+    .find()
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1,
+      _id: 0,
+    })
+    .toArray();
+
+  res.send({ contests });
 });
 
-// router.get("/contest")
+router.get("/contest/:contestId", async (req, res) => {
+  const client = await connectClient();
+
+  const contest = await client
+    .collection("contests")
+    .findOne({ id: req.params.contestId });
+
+  res.send({ contest });
+});
 
 export default router;
