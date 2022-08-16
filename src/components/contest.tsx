94e444168e35f2ab4899fdc96f1405bd48ff59c7
Samer Buna <samerbuna@users.noreply.github.com>
2022-08-15 18:20:45 -0700

    5.1

--- src/components/contest.tsx
+++ src/components/contest.tsx
@@ -27,6 +27,21 @@ const Contest = ({ initialContest, onContestListClick }) => {
         <div className="title">Contest Description</div>
         <div className="description">{contest.description}</div>
 
+        <div className="title">Proposed Names</div>
+        <div className="body">
+          {contest.names?.length > 0 ? (
+            <div className="list">
+              {contest.names.map((proposedName) => (
+                <div key={proposedName.id} className="item">
+                  {proposedName.name}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No names proposed yet</div>
+          )}
+        </div>
+
         <a
           href="/"
           className="link"
